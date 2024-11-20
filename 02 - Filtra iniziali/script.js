e68a19f8cc14983230166db80040efc088d68fcf
PatrizioFiori <patrizio.fiori1995@gmail.com>
2024-11-20 13:45:40 +0100

    02 - completo

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -5,9 +5,18 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+const inizianoPerA = (names) => {
+    let inizialiArray = []
+    for (let i = 0; i < names.length; i++)
+        if (names[i][0] == "A"){
+            inizialiArray.push(names[i]) 
+        }
+    return inizialiArray
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(inizianoPerA (names));
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
