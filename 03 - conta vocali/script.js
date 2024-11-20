604143fb6dcd64da5bc707ed69edc1730c56eb81
PatrizioFiori <patrizio.fiori1995@gmail.com>
2024-11-20 14:33:27 +0100

    03 - Add switch statement

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -5,11 +5,36 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+// VERSIONE SWITCH STATEMENT
+const vocaliPresentiNellaParola = (word) => {
+    let numeroDiVocali = 0;
+
+    for (let i = 0; i < word.length; i++) {
+        switch (word[i].toLowerCase()) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                numeroDiVocali++; 
+                break;
+            default:
+                break;
+        }
+    }
+
+    return numeroDiVocali;
+}
+
+
+// VERSIONE CON IF 
+/* 
 const vocaliPresentiNellaParola = (word) => {
 let numeroDiVocali = 0
 let arrayvocali = []
 
 for (let i = 0; i < word.length; i++){
+
     if (word[i] == "a" || word[i] == "e" || word[i] == "i" || word[i] == "o" || word[i] == "u"){
         numeroDiVocali++
 
@@ -17,6 +42,10 @@ for (let i = 0; i < word.length; i++){
     }
     return numeroDiVocali
 }
+    */
+
+
+
 
 
 // Invoca la funzione qui e stampa il risultato in console
