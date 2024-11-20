6dcac6571c646504db58cf24392715969e59a78d
PatrizioFiori <patrizio.fiori1995@gmail.com>
2024-11-20 14:02:12 +0100

    03 - completo

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -5,9 +5,23 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+const vocaliPresentiNellaParola = (word) => {
+let numeroDiVocali = 0
+let arrayvocali = []
+
+for (let i = 0; i < word.length; i++){
+    if (word[i] == "a" || word[i] == "e" || word[i] == "i" || word[i] == "o" || word[i] == "u"){
+        numeroDiVocali++
+
+    }
+    }
+    return numeroDiVocali
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(vocaliPresentiNellaParola(word));
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
